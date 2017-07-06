ae11c67329561de0b2ad53c4873e27b7cfcce088
U. Mariano Gálvez <alumno@labs.academico>
2017-07-05 19:51:00 -0600

    Initial commit

---
+++ src/com/umg/programacion/Canal.java
@@ -0,0 +1,7 @@
+package com.umg.programacion;
+
+/**
+ * Created by alumno on 5/07/2017.
+ */
+public class Canal {
+}
---
+++ src/com/umg/programacion/Tv.java
@@ -0,0 +1,12 @@
+package com.umg.programacion;
+
+/**
+ * Created by alumno on 5/07/2017.
+ */
+public class Tv {
+private String marca;
+private int pulgadas;
+private String caracteristica;
+
+
+}
